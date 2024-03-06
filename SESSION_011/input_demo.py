36492c32ea2d0a173d43f088a3051ba49ee2c48b
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-06 23:03:35 +0530

    Add files via upload

---
+++ SESSION_011/input_demo.py
@@ -0,0 +1,15 @@
+print("Start of the program")
+
+s1=input("Enter some text:")
+print("Entered text:",s1)
+print("type(s1):",type(s1))
+
+s2=input("Enter an integer:")
+print("Entered integer:",s2)
+print("type(s2)",type(s2))
+
+s3=input("Enter the fractional number:")
+print("Enter the fractional number:",s3)
+print("type(s3)",type(s3))
+
+print("End of program")
