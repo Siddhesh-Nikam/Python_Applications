42f2fee572ff8b29014806efd66dc486bf432b06
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-08 11:13:43 +0530

    Add files via upload

---
+++ SESSION_014/determine_the_sign.py
@@ -0,0 +1,12 @@
+s1 = input("Enter an integer:")
+n = int(s1)
+
+if n<0:
+    print("Given number is negative")
+
+elif n>0:
+    print("Given number is positive")
+
+else:
+    print("Given number is 0")
+    
---
+++ SESSION_014/while_demo.py
@@ -0,0 +1,9 @@
+print("Start of program")
+
+i = 0 
+
+while i < 5:
+    print("Value of i is:",i)
+    i = i + 1
+
+print("End of program")
