73071c13aba3688ced992dd1c90fc35d60e88cd7
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-08 11:03:47 +0530

    Add files via upload

---
+++ SESSION_013/if_else_statement.py
@@ -0,0 +1,15 @@
+print("Start of program")
+
+s1=input("Enter first integer:")
+n1 = int(s1)
+
+s2=input("Enter second integer:")
+n2 = int(s2)
+
+if n1<n2:
+    print(n1 , "is smaller than" , n2)
+
+else:
+    print(n1 , "is greater than" , n2)
+
+print("End of the program")
