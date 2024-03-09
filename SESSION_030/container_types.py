68e51befadd471a6f251100761d645968f1ad57b
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-09 13:20:08 +0530

    Add files via upload

---
+++ SESSION_030/container_types.py
@@ -0,0 +1,24 @@
+s="Hello"
+T=(10,20,30,40)
+L=[100,200,300,400]
+D={'a':1000,'b':2000,'c':3000}
+S={-1,-2,-3}
+
+for c in s:
+    print(s)
+
+for x in T:
+    print(x)
+
+for y in L:
+    print(y)
+
+for key in D:
+    print(key,D[key])
+
+for element in S:
+    print(element)
+    
+
+
+    
