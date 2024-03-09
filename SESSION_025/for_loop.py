ab7f7d044d57a8887e9529720066e21afa003847
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-09 11:11:21 +0530

    Add files via upload

---
+++ SESSION_025/for_loop.py
@@ -0,0 +1,21 @@
+print("Start a program")
+
+print("Start of first loop")
+
+L = [100,200,300,400,500]
+
+for x in L:
+    print(x,type(x))
+print("End of last loop")
+
+print("Start of second for loop")
+
+T = (True,10,(100,200),{'a':1.1,'b':2.2},3.7)
+
+for x in T:
+    print(x,type(x))
+print("End of second for loop")
+
+print("End of program")
+
+
