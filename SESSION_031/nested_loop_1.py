79d33baa0ea00ace1e920e07f292a1ec12f18f19
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-15 11:35:19 +0530

    Add files via upload

---
+++ SESSION_031/nested_loop_1.py
@@ -0,0 +1,10 @@
+print("Start of program")
+i=0
+while i<5:
+    print("i",i)
+    j=0
+    while j<3:
+        print("j",j)
+        j=j+1
+    i=i+1
+print("End of program")
---
+++ SESSION_031/pattern_1.py
@@ -0,0 +1,10 @@
+print("Start of program") 
+i = 0
+while i < 6:
+    print("i:", i)
+    j = 0
+    while j < i:
+        print("j:", j)
+        j = j + 1
+    i = i + 1 
+print("End of program")
